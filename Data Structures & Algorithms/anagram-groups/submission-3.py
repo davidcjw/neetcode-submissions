9a4ecae45367d683a837e84c2de122f2262c0ad3
David Chong <davidcjw@gmail.com>
2026-07-09 13:09:22 +0800

    Add: anagram-groups - submission-3

---
+++ Data Structures & Algorithms/anagram-groups/submission-3.py
@@ -0,0 +1,21 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # Solution 1: sorting
+        # Time: n * k log k
+        # Space: n * k
+        # res = defaultdict(list)
+        # for s in strs:
+        #     res["".join(sorted(s))].append(s)
+        # return list(res.values())
+
+        # Solution 2: Using fixed size list
+        # Time:
+        # Space:
+        res = defaultdict(list)
+        for s in strs:
+            key = [0] * 26
+            for char in s:
+                key[ord(char) - ord('a')] += 1
+            res[tuple(key)].append(s)
+
+        return list(res.values())
