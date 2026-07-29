9af331997654732fb3780a475ad9cd46bc55a28f
David Chong <davidcjw@gmail.com>
2026-07-29 14:13:31 +0800

    Add: daily-temperatures - submission-0

---
+++ Data Structures & Algorithms/daily-temperatures/submission-0.py
@@ -0,0 +1,14 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        # brute force
+        res = []
+        for i in range(len(temperatures)):
+            curr = temperatures[i]
+            for j in range(i+1, len(temperatures)):
+                if temperatures[j] > curr:
+                    res.append(j-i)
+                    break
+            if len(res) != i+1:
+                res.append(0)
+        
+        return res
